01a1b24c5f59f4ba0e1791fc3bebc05039e0e4fb
sumit <sumit.patel@cbnits.com>
2019-12-23 11:34:51 +0530

    added utility for converting numbers

--- pikachu/utils/ternary_to_decimal.c
+++ pikachu/utils/ternary_to_decimal.c
@@ -1,19 +1,24 @@
 
 #include<stdio.h>
-#include "stdio.h"
+#include<stdlib.h>
+#include<string.h>
+#include "../src/utils.h"
 
 int main()
 {
-  int ch;
+  char ch;
   while(1)
   {
-    scanf("%c ", &ch);
+    char input[80];
+    printf("%s\n", "1: ternary_to_decimal");
+    printf("%s\n", "2: decimal_to_ternary");
+    printf("%s\n", "q: quit");
+    scanf("%c \n", &ch);
     switch(ch)
     {
-      case '1':
-      case '2':
-      case 'q': break;
+      case '1': fgets(input, 80, stdin); printf("%d\n", ternary_to_decimal(input, strlen(input)-1)); break;
+      case '2': fgets(input, 80, stdin); int num = atoi(input); decimal_to_ternary(num, input, 9); input[9] = '\0'; printf("%s\n", input); printf("%s\n", "123456789");break;
+      case 'q': return 0;
     }
   }
-  return 0;
 }
