7c369468d095c7c6c297edf842e1e54575381da8
sumit <sumit.patel@cbnits.com>
2019-12-23 17:27:49 +0530

    mvi test added for every register

--- pikachu/test/test_mvi.c
+++ pikachu/test/test_mvi.c
@@ -24,70 +24,134 @@ machine *test_program(char *buffer, int length)
   return m;
 }
 
-int test_mvi_A_0()
+int test_mvi(char *buffer, int reg, int value)
 {
-  fprintf(stderr, "Copying 0 to A...\n");
-  char *buffer = "PI PI PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA";
+  printf("------------------------------------------------------------------------------------------------------------------\n");
+  if(reg > 6 || reg < 0)
+  {
+    fprintf(stderr, "%s\n", "INVALID_REG");
+    return 1;
+  }
+  if(reg == 6)
+  {
+    fprintf(stderr, "Copying %d to memory...\n", value);
+  }
+  else
+  {
+    fprintf(stderr, "Copying %d to %c\n", value, reg+65);
+  }
   int l;
 
   machine *m = test_program(buffer, strlen(buffer));
 
-  printf("%s\n", "Checking the registers values");
-  if(ternary_to_decimal(m -> R[0], 9) != 0)
+  if(reg == 6)
   {
-    fprintf(stderr, "%s%d\n", "A not 0 :", ternary_to_decimal(m -> R[0], 9));
-    return 1;
+    printf("%s\n", "Checking the value at memory");
+    if(ternary_to_decimal(m->M + ternary_to_decimal(m -> R[1], 9) + 9841, 9) != value)
+    {
+      fprintf(stderr, "memory not %d: %d\n", value, ternary_to_decimal(m -> R[0], 9));
+      printf("------------------------------------------------------------------------------------------------------------------\n");
+      return 2;
+    }
+    else
+    {
+      fprintf(stderr, "[✓] Succesfull!\n");
+    }
   }
   else
   {
-    fprintf(stderr, "[✓] Succesfull!\n");
+    printf("%s\n", "Checking the registers values");
+    if(ternary_to_decimal(m -> R[reg], 9) != value)
+    {
+      fprintf(stderr, "%c not %d: %d\n", reg+65, value, ternary_to_decimal(m -> R[0], 9));
+      printf("------------------------------------------------------------------------------------------------------------------\n");
+      return 2;
+    }
+    else
+    {
+      fprintf(stderr, "[✓] Succesfull!\n");
+    }
   }
-  delete_machine(m);
-  return 0;
-}
-
-int test_mvi_A_13()
-{
-  fprintf(stderr, "Copying 13 to A...");
-  char *buffer = "";
-  int l;
-  machine *m = test_program(buffer, 10);
-
-  if(ternary_to_decimal(m -> R[0], 9) != 13)
-  {
-    fprintf(stderr, "%s%d\n", "A not 0 :", ternary_to_decimal(m -> R[0], 9));
-    return 1;
-  }
-  else
-  {
-    fprintf(stderr, "Succesfull!\n");
-  }
-  delete_machine(m);
-  return 0;
-}
-
-int test_mvi_A_n1231()
-{
-  fprintf(stderr, "Copying -1231 to A...");
-  char *buffer = "";
-  int l;
-  machine *m = test_program(buffer, 10);
-
-  if(ternary_to_decimal(m -> R[0], 9) != -1231)
-  {
-    fprintf(stderr, "%s%d\n", "A not 0 :", ternary_to_decimal(m -> R[0], 9));
-    return 1;
-  }
-  else
-  {
-    fprintf(stderr, "Succesfull!\n");
-  }
+  printf("------------------------------------------------------------------------------------------------------------------\n");
   delete_machine(m);
   return 0;
 }
 
 int test_mvi_main()
 {
-  test_mvi_A_0();
+  printf("Testing MVI instruction\n");
+
+  /*test A register with 0 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA", 0, 0);
+
+  /*test A register with 13 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKACHU PIKACHU PIKACHU", 0, 13);
+
+  /*test A register with -1231 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKA PIKA PIKA PI PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PI", 0, -1231);
+
+  /*test B register with 0 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKA PIKACHU PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA", 1, 0);
+
+  /*test B register with 13 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKA PIKACHU PIKA PIKA PIKA PIKA PIKA PIKA PIKACHU PIKACHU PIKACHU", 1, 13);
+
+  /*test B register with -1231 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKA PIKACHU PIKA PI PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PI", 1, -1231);
+
+  /*test C register with 0 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PI PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA", 2, 0);
+
+  /*test C register with 13 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PI PIKA PIKA PIKA PIKA PIKA PIKA PIKACHU PIKACHU PIKACHU", 2, 13);
+
+  /*test C register with -1231 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PI PIKA PI PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PI", 2, -1231);
+
+  /*test D register with 0 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA", 3, 0);
+
+  /*test D register with 13 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKACHU PIKACHU PIKACHU", 3, 13);
+
+  /*test D register with -1231 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PIKA PIKA PI PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PI", 3, -1231);
+
+  /*test E register with 0 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PIKACHU PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA", 4, 0);
+
+  /*test E register with 13 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PIKACHU PIKA PIKA PIKA PIKA PIKA PIKA PIKACHU PIKACHU PIKACHU", 4, 13);
+
+  /*test E register with -1231 value
+  */
+  test_mvi("PI PI PIKA PIKA PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PI", 4, -1231);
+
+  /*test F register with 0 value
+  */
+  test_mvi("PI PI PIKA PIKACHU PI PI PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA PIKA", 5, 0);
+
+  /*test F register with 13 value
+  */
+  test_mvi("PI PI PIKA PIKACHU PI PI PIKA PIKA PIKA PIKA PIKA PIKA PIKACHU PIKACHU PIKACHU", 5, 13);
+
+  /*test F register with -1231 value
+  */
+  test_mvi("PI PI PIKA PIKACHU PI PI PIKA PI PIKACHU PIKACHU PIKA PI PIKACHU PIKACHU PI", 5, -1231);
+
   return 0;
 }
--- pikachu/utils/ternary_to_decimal.c
+++ pikachu/utils/ternary_to_decimal.c
@@ -19,7 +19,7 @@ int main()
     {
       case '1': fgets(input, 80, stdin); printf("%d\n", ternary_to_decimal(input, strlen(input)-1)); break;
       case '2': fgets(input, 80, stdin); int num = atoi(input); decimal_to_ternary(num, input, 9); input[9] = '\0'; printf("%s\n", input); printf("%s\n", "123456789");break;
-      case '3': fgets(input, 80, stdin); int i; for(i=0; i < strlen(input)-1; i++) if(input[i] == '-') printf("PI ");else if(input[i] == '0') printf("PIKA "); else printf("PIKACHU"); printf("\n"); break;
+      case '3': fgets(input, 80, stdin); int i; for(i=0; i < strlen(input)-1; i++) if(input[i] == '-') printf("PI ");else if(input[i] == '0') printf("PIKA "); else printf("PIKACHU "); printf("\n"); break;
       case 'q': return 0;
     }
   }
